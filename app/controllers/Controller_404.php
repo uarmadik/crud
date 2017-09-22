36f200b439968b4942133d0e4d212649e2f1a9cb
Ihor Harmider <uarmadik@gmail.com>
2017-09-22 11:53:24 +0300

    add function index and removed construct

--- app/controllers/Controller_404.php
+++ app/controllers/Controller_404.php
@@ -7,7 +7,7 @@ use app\core\View;
 
 class Controller_404
 {
-    public function __construct()
+    public function index()
     {
         $view = new View();
         $view->generate('404.html.twig', null);
