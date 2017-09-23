79ae7b409e22ccd478150cca6c0dd47c6e58e738
Ihor Harmider <uarmadik@gmail.com>
2017-09-23 23:32:59 +0300

    modified

--- app/controllers/Controller_main.php
+++ app/controllers/Controller_main.php
@@ -9,15 +9,9 @@ use app\models;
 
 class Controller_main
 {
-    protected $data = [
-        ['id'=>'1','header'=>'header 1', 'text'=>'text 1'],
-        ['id'=>'2','header'=>'header 2', 'text'=>'text 2'],
-        ['id'=>'3','header'=>'header 3', 'text'=>'text 3'],
-        ['id'=>'4','header'=>'header 4', 'text'=>'text 4'],
-        ['id'=>'5','header'=>'header 5', 'text'=>'text 5'],
-        ['id'=>'6','header'=>'header 6', 'text'=>'text 6'],
-    ];
-
+    /**
+     * Return view with all posts from db;
+     */
     public function index()
     {
         $db = new models\Model_posts();
